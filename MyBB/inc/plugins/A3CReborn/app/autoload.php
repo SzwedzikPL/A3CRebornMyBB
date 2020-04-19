151b597e4055b4b6dfae24d1660a4dd728d107dd
Michal Pietryka <michalpietryka@outlook.com>
2020-04-19 18:26:26 +0200

    add namespaces autoload

--- MyBB/inc/plugins/A3CReborn/app/autoload.php
+++ MyBB/inc/plugins/A3CReborn/app/autoload.php
@@ -1,4 +1,7 @@
 <?php
 
-require_once 'Application.php';
-require_once 'Response.php';
+spl_autoload_register(function($className) {
+    str_replace("\\", DIRECTORY_SEPARATOR, $className);
+    $className = ltrim($className, 'A3C');
+    require_once __DIR__ . DIRECTORY_SEPARATOR . $className . '.php';
+});
