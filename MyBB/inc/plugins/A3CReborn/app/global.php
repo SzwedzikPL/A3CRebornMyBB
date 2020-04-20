3334bb4e8bcee758ed0ef758e06743abbc13dab2
Dawid Szwed <szwedzikpl@gmail.com>
2020-04-20 16:08:14 +0200

    Update plugin info

--- MyBB/inc/plugins/A3CReborn/app/global.php
+++ MyBB/inc/plugins/A3CReborn/app/global.php
@@ -2,9 +2,9 @@
 
 define("PLUGIN_INFO", array(
   'name' => 'A3CReborn',
-  'description' => '',
-  'website' => 'https://arma3coop.pl',
-  'author' => 'Arma3Coop.pl',
+  'description' => 'Modyfikacja MyBB na potrzeby społeczności Arma3Coop.pl',
+  'website' => 'https://github.com/SzwedzikPL/A3CRebornMyBB',
+  'author' => 'SzwedzikPL & Harven',
   'authorsite' => 'https://github.com/SzwedzikPL/A3CRebornMyBB',
   'version' => '0.0.1',
   'compatibility' => '18*',
